1c5acebb000820887fcbab5dcf58889cbe7aef16
Oyewande Oluwaseun <seunemmanuel2255@gmail.com>
2022-03-11 11:43:37 +0100

    Hello World Contract

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -2,22 +2,22 @@
 pragma solidity ^0.5.0;
 
 contract HelloWorld {
+    string public name;
+    uint256 public balance = 1000;
 
-    constructor () public {}
-    
-    string public name = "Hello World";
-    uint public balance = 1000;
+    constructor() public {
+        name = "Hello World";
+    }
 
-    function setBalance(uint value) public returns(uint) {
+    function setBalance(uint256 value) public returns (uint256) {
         balance = value;
     }
 
-    function getBalance() public view returns(uint) {
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
-    function getName() public view returns(string memory) {
+    function getName() public view returns (string memory) {
         return name;
     }
-
 }
